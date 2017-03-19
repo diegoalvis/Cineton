b15f600fb6dd56c2856fb02f9820e64e8ff91cb8
diegoalvispal <diegoalvispal@gmail.com>
2017-03-18 19:32:00 -0500

    add Movie Detail model

---
+++ app/src/main/java/com/diegoalvis/android/cintefon/models/MovieDetail.java
@@ -0,0 +1,10 @@
+package com.diegoalvis.android.cintefon.models;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by diegoalvis on 3/18/17.
+ */
+
+public class MovieDetail extends BaseObservable {
+}
